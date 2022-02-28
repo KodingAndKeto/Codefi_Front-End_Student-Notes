5f0b5ccaf002c672333fbb053f7cd4b80fcf22f2
Koding and Keto <88945347+KodingAndKeto@users.noreply.github.com>
2022-02-28 14:59:37 -0600

    Codefi Code
    Codefi Code

--- C04__Building-Real-Things__JavaScript/~Starter Code/js/crazy-buttons.js
+++ C04__Building-Real-Things__JavaScript/~Starter Code/js/crazy-buttons.js
@@ -1,7 +1,27 @@
 // * ========== HTML Element Selectors ========== * \\
-
+const crazyButton = document.querySelector(".crazy-button");
+const crazyButton = document.querySelector("crazy.button2");
 // * ========== Function Declarations ========== * \\
 // FUNCTION: TSK
+function goBtnWild(){
+    const offsetTop = Math.random() * (window.innerHeight - crazyButton.clientHeight);
+    const offsetLeft = Math.random() * (window.innerWidth - crazyButton.clientWidth);
 
+    crazyButton.style.top = offsetTop + "px";
+    crazyButton.style.left = offsetLeft + "px";
+    
+    
+}
+
+function goBtnWildAgain(){
+    const offsetTop = Math.random() * (window.innerHeight - crazyButton2.clientHeight);
+    const offsetLeft = Math.random() * (window.innerWidth - crazyButton2.clientWidth);
+
+    crazyButton2.style.top = offsetTop + "px";
+    crazyButton2.style.left = offsetLeft + "px";
+}
+    
 // * ========== Event Listeners ========== * \\
 // EVENT LISTENER: TSK
+crazyButton.addEventListener("mouseenter",goBtnWild);
+crazyButton2.addEventListener("mouseenter",goBtnWild);
